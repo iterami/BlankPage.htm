9103d7580689ffe318edb1b3fb682ca8e8a9f662
Jan Hořava <czechinthemail@gmail.com>
2026-03-30 03:13:03 -0700

    switched to globalThis.onload

--- repo.js
+++ repo.js
@@ -1,5 +1,5 @@
 'use strict';
 
-window.onload = function(){
+globalThis.onload = function(){
     document.body.style.backgroundColor = '#' + (globalThis.location.search.substring(1) || '000');
 };
